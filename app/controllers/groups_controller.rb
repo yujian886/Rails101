11d4d1d9ea0d7fd2434e1ba5c20088725fb7d5b7
yujian886 <yjxuexi@gmail.com>
2021-01-21 20:59:04 +0800

    add validation to title apply to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,8 +22,11 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
+    if @group.save
     redirect_to groups_path
+  else
+    render :new
+    end
   end
     def destroy
       @group = Group.find(params[:id])
@@ -31,6 +34,7 @@ class GroupsController < ApplicationController
       flash[:alert] = "Group deleted"
       redirect_to groups_path
   end
+
   private
 
   def  group_params
